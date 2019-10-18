2dd0bb197b4bbb7706ccbf2ba8dfa28f01f6a5d1
andrav24 <andrav24@yandex.ru>
2019-10-18 19:44:54 +0700

    Delete unused methods.

--- app/src/main/java/tech/andrav/loftmoney/MainActivity.java
+++ app/src/main/java/tech/andrav/loftmoney/MainActivity.java
@@ -103,29 +103,4 @@ public class MainActivity extends AppCompatActivity {
             return 2;   // return number of tabs
         }
     }
-
-    @Override
-    protected void onPause() {
-
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onStart() {
-
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-
-        super.onStop();
-    }
-
-
 }
